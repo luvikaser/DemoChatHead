13a59d03c2025fffe77777aded2e119de9474d0f
nhutlm2 <leminhnhut95@gmail.com>
2017-03-14 17:52:59 +0700

    update onMeasure() in UpArrowLayout

--- app/src/main/java/nhutlm2/fresher/demochathead/ChatHeadUI/PopupFragment/UpArrowLayout.java
+++ app/src/main/java/nhutlm2/fresher/demochathead/ChatHeadUI/PopupFragment/UpArrowLayout.java
@@ -19,7 +19,6 @@ import nhutlm2.fresher.demochathead.R;
 
 public class UpArrowLayout extends ViewGroup {
     private final Point pointTo = new Point(0, 0);
-    private final ArrayList<View> mMatchParentChildren = new ArrayList<View>(1);
     private ImageView arrowView;
     private int arrowDrawable = R.drawable.chat_top_arrow;
 
@@ -35,15 +34,6 @@ public class UpArrowLayout extends ViewGroup {
 
     public UpArrowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
-    }
-
-    public int getArrowDrawable() {
-        return arrowDrawable;
-    }
-
-    public void setArrowDrawable(int arrowDrawable) {
-        this.arrowDrawable = arrowDrawable;
         init();
     }
 
@@ -66,109 +56,12 @@ public class UpArrowLayout extends ViewGroup {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int count = getChildCount();
         int measureSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
         arrowView.measure(measureSpec, measureSpec);
-        int arrowViewMeasuredHeight = arrowView.getMeasuredHeight();
-        int size = MeasureSpec.getSize(heightMeasureSpec);
-        if (size > arrowViewMeasuredHeight) {
-            size -= arrowViewMeasuredHeight + pointTo.y;
-            heightMeasureSpec = MeasureSpec.makeMeasureSpec(size, MeasureSpec.getMode(heightMeasureSpec));
-        }
-
-
-        final boolean measureMatchParentChildren =
-                MeasureSpec.getMode(widthMeasureSpec) != MeasureSpec.EXACTLY ||
-                        MeasureSpec.getMode(heightMeasureSpec) != MeasureSpec.EXACTLY;
-
-        int maxHeight = 0;
-        int maxWidth = 0;
-        int childState = 0;
-
-        for (int i = 0; i < count; i++) {
-            final View child = getChildAt(i);
-            if (child == arrowView) continue;
-            if (child.getVisibility() != GONE) {
-                measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, 0);
-                final LayoutParams lp = (LayoutParams) child.getLayoutParams();
-                maxWidth = Math.max(maxWidth,
-                        child.getMeasuredWidth());
-                maxHeight = Math.max(maxHeight,
-                        child.getMeasuredHeight());
-                childState = combineMeasuredStates(childState, child.getMeasuredState());
-                if (measureMatchParentChildren) {
-                    if (lp.width == LayoutParams.MATCH_PARENT ||
-                            lp.height == LayoutParams.MATCH_PARENT) {
-                        mMatchParentChildren.add(child);
-                    }
-                }
-            }
-        }
-
-
-        // Check against our minimum height and width
-        maxHeight = Math.max(maxHeight, getSuggestedMinimumHeight());
-        maxWidth = Math.max(maxWidth, getSuggestedMinimumWidth());
-
-
-        setMeasuredDimension(resolveSizeAndState(maxWidth, widthMeasureSpec, childState),
-                resolveSizeAndState(maxHeight, heightMeasureSpec,
-                        childState << MEASURED_HEIGHT_STATE_SHIFT));
-
-        count = mMatchParentChildren.size();
-        if (count > 1) {
-            for (int i = 0; i < count; i++) {
-                final View child = mMatchParentChildren.get(i);
-
-                final MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
-                int childWidthMeasureSpec;
-                int childHeightMeasureSpec;
-
-                if (lp.width == LayoutParams.MATCH_PARENT) {
-                    childWidthMeasureSpec = MeasureSpec.makeMeasureSpec(getMeasuredWidth() -
-                                    lp.leftMargin - lp.rightMargin,
-                            MeasureSpec.EXACTLY);
-                } else {
-                    childWidthMeasureSpec = getChildMeasureSpec(widthMeasureSpec,
-
-                            lp.leftMargin + lp.rightMargin,
-                            lp.width);
-                }
-
-                if (lp.height == LayoutParams.MATCH_PARENT) {
-                    childHeightMeasureSpec = MeasureSpec.makeMeasureSpec(getMeasuredHeight() -
-
-                                    lp.topMargin - lp.bottomMargin,
-                            MeasureSpec.EXACTLY);
-                } else {
-                    childHeightMeasureSpec = getChildMeasureSpec(heightMeasureSpec,
-
-                            lp.topMargin + lp.bottomMargin,
-                            lp.height);
-                }
-
-                child.measure(childWidthMeasureSpec, childHeightMeasureSpec);
-            }
-        }
-
-
-        setMeasuredDimension(getMeasuredWidth(), getMeasuredHeight() + arrowViewMeasuredHeight + pointTo.y);
         updatePointer();
-
     }
 
-    protected void measureChildWithMargins(View child,
-                                           int parentWidthMeasureSpec, int widthUsed,
-                                           int parentHeightMeasureSpec, int heightUsed) {
-        final LayoutParams lp = child.getLayoutParams();
 
-        final int childWidthMeasureSpec = getChildMeasureSpec(parentWidthMeasureSpec, widthUsed, lp.width);
-        final int childHeightMeasureSpec = getChildMeasureSpec(parentHeightMeasureSpec,
-
-                +heightUsed, lp.height);
-
-        child.measure(childWidthMeasureSpec, childHeightMeasureSpec);
-    }
 
     public void pointTo(final int viewX, final int viewY) {
         pointTo.x = viewX;
@@ -201,7 +94,6 @@ public class UpArrowLayout extends ViewGroup {
             if (child == arrowView) continue;
             child.layout(left, top + arrowView.getMeasuredHeight() + pointTo.y, right, bottom);
         }
-
     }
     protected LayoutParams generateDefaultLayoutParams() {
         return new FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
